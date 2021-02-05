67f905389b1ffdd001ec9f6b17063ed3c730d0ed
Eniyaraj <76511507+Eniyaraj@users.noreply.github.com>
2021-02-05 10:19:07 +0400

    Add files via upload

---
+++ Roof.js
@@ -0,0 +1,22 @@
+class Roof{
+    constructor(x,y,options){
+        var options ={
+            'isStatic' : false,
+            'restitution' : 0.3,
+            'friction' : 0.5,
+            'density' : 1.2
+        };
+        this.body = Bodies.rectangle(x,y,50,50, options);
+        this.width = 50;
+        this.height = 50;
+        World.add(world, this.body);
+    };
+    display(){
+        push();
+        stroke("black");
+        fill("#D8D8D8");
+        rectMode(CENTER);
+        rect(0,0, this.width, this.height);
+        pop();
+    }
+}
